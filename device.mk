5a29cdf7caeb8b48b00a008a6069577db782d05d
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2021-08-02 08:38:00 +0800

    spes: overlay: Add NotchBar Killer overlay
     * This overlay gets rid of the black bar in most apps/games, and lets the screen fill past the notch.
    
    Change-Id: Ic008ce5d569127d230dc44451929477e063c4471
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: isnrdra <isanoorendra@gmail.com>
    Signed-off-by: indraadp <indraproject777@gmail.com>

--- device.mk
+++ device.mk
@@ -383,6 +383,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_k7tn/android.hardware.se.omapi.ese.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_k7tn/com.android.nfc_extras.xml
 
+# NotchBar Killer overlay
+PRODUCT_PACKAGES += \
+    NotchBarKiller \
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
