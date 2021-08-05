94952ccef75805781866dc1ad78b158a40ed2662
Danny Lin <danny@kdrag0n.dev>
2021-08-05 01:51:10 -0700

    spes: props: Enable Gboard side padding
    This adds padding to the left and right sides of the keyboard when using
    Gboard.
    
    In portrait mode, there is 4dp of padding to align the keyboard with
    some UI elements and separate the keys from the edge of the screen.
    
    In landscape mode, there is 64dp of padding to reduce the overall key
    width, which makes the keyboard significantly easier to use.

--- device.mk
+++ device.mk
@@ -232,6 +232,13 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-custom \
     fastbootd
 
+# Gboard side padding
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.ime.kb_pad_port_l=4 \
+    ro.com.google.ime.kb_pad_port_r=4 \
+    ro.com.google.ime.kb_pad_land_l=64 \
+    ro.com.google.ime.kb_pad_land_r=64
+
 # GNSS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1-service-qti
