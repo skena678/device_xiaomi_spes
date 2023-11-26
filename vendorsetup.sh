92916c83398c33298cfffa6609749dcd74ec5d81
Agatha senpai <naufalmarjan2014@gmail.com>
2023-11-26 22:42:09 +0000

    spes: Track mi-sound configs repository

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,3 +12,4 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 # ViperX
 git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX --depth=1
 git clone https://github.com/kenway214/vendor_bcr.git vendor/bcr
+git clone https://github.com/muralivijay/vendor_xiaomi_spes-misound vendor/xiaomi/spes-misound
