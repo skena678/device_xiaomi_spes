4f425da468f690fc7864f41168c130289fa5f5db
Agatha senpai <naufalmarjan2014@gmail.com>
2023-12-08 02:33:22 +0000

    spes: Welcome to Derpfest AOSP ify

--- derp_spes.mk
+++ derp_spes.mk
@@ -11,32 +11,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from spes/spesn device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-# Inherit some common Nusantara stuff.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+# Inherit some common DerpFest stuff
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
-
-# Nusantara stuff.
-NAD_BUILD_TYPE := OFFICIAL
+# Rom Specific Flags
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_USES_BLUR := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP  := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_ENABLE_BLUR := true
 USE_PIXEL_CHARGING := true
 
-# GMS
-TARGET_GAPPS_ARCH := arm64
-USE_GAPPS := true
-
 # Product Specifics
-PRODUCT_NAME := nad_spes
+PRODUCT_NAME := derp_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-nad
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
